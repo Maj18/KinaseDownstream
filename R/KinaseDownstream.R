481c05257977e143f35ac8d46ce41d56ef7efba5
Yuan Li <Yuan.Li@nbis.se>
2025-06-04 12:59:16 +0200

    Update R script

--- R/KinaseDownstream.R
+++ R/KinaseDownstream.R
@@ -151,6 +151,7 @@ prepInput4PhosNetVis = function(pair, limma_output, PTMsubstrates4PTMSEAanalysis
   limma_output$PTM.FlankingRegion2 = paste0(limma_output$PTM.FlankingRegion, "-p")
   limma_output = limma_output %>%
       filter(Phosphosite%in%PTMsubstrates4PTMSEAanalysis)
+  head(limma_output)
   limma_output$uniprotID = limma_output$Phosphosite %>% sub("_.*", "", .)
   limma_output$PhosLocation = limma_output$Phosphosite %>% sub(".*_", "", .)
   limma_output = limma_output%>% 
