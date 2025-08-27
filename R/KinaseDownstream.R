9be0db5b69d89d8efdecf16d7cf135493f60c0f5
Yuan Li <Yuan.Li@nbis.se>
2025-08-27 12:20:16 +0200

    Update processPTMSEAresult

--- R/KinaseDownstream.R
+++ R/KinaseDownstream.R
@@ -189,7 +189,8 @@ processPTMSEAresult = function(PTMSEA_FILE_PATH, output.score.type = "NES", sig.
   fdrs = grep("fdr.pvalue", colnames(gct_data), value=T)
   for (fdr in fdrs) {
 	  N = gsub("fdr.pvalue.", "", fdr)
-	  kinaseList = gct_data %>% filter(gct_data[,fdr,drop=T]<sig.thresh) %>% pull(id) %>% 
+	  kinaseList = gct_data[,c("id",fdr)] %>% na.omit() %>%
+      filter(gct_data[,fdr,drop=T]<sig.thresh) %>% pull(id) %>% 
 		  grep("KINASE", ., value=T) %>% gsub("KINASE-PSP_", "", .) %>% 
 		  gsub("KINASE-iKiP_", "", .) %>% stringr::str_split(., "/|[.]") %>% 
 		  unlist() %>% gsub("-.*", "", .) %>% paste0(., collapse=",")
@@ -205,7 +206,7 @@ processPTMSEAresult = function(PTMSEA_FILE_PATH, output.score.type = "NES", sig.
   fdrs = grep("fdr.pvalue", colnames(gct_data), value=T)
   for (fdr in fdrs) {
 	  N = gsub("fdr.pvalue.", "", fdr)
-	  kinaseList = gct_data %>% pull(id) %>% 
+	  kinaseList = gct_data[,c("id",fdr)] %>% na.omit() %>% pull(id) %>% 
 		  grep("KINASE", ., value=T) %>% gsub("KINASE-PSP_", "", .) %>% 
 		  gsub("KINASE-iKiP_", "", .) %>% stringr::str_split(., "/|[.]") %>% 
       unlist() %>% gsub("-.*", "", .) %>% paste0(., collapse=",")
